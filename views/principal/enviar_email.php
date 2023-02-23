44fa4b91d036c1cc1d6565cb12203d2de83a236f
Alejandro <99648035+amartingonz@users.noreply.github.com>
2023-02-23 21:50:33 +0100

    Update enviar_email.php

--- views/principal/enviar_email.php
+++ views/principal/enviar_email.php
@@ -22,7 +22,7 @@ try {
     $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
     $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
     $mail->Username   = 'perrobuenpito@gmail.com';                     //SMTP username
-    $mail->Password   = 'klwzmhybvhqrwyof';                               //SMTP password
+    $mail->Password   = '';                               //SMTP password
     $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
     $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
 
